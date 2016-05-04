3812a79e3f094ba1d4b7fe616edc9cbb933d5073
Lukas <Lukasbarry@yahoo.com>
2016-05-04 11:20:31 -0400

    fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160503191001) do
 
   create_table "users", force: :cascade do |t|
@@ -31,8 +30,5 @@ ActiveRecord::Schema.define(version: 20160503191001) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-=======
-ActiveRecord::Schema.define(version: 0) do
->>>>>>> 3cc84219733b6de62b0d609330c4ca94a502079e
 
 end
