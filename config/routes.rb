b97a20fee248f6b1ee19206f9770f48dea4689ac
Brian McDonald <bmcdonald05@gmail.com>
2016-05-11 10:01:47 -0400

    reconfigured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get 'show' => 'buyers#show_buyers'
-  get 'new' => 'buyers#new'
-  put 'forms' => 'buyers#create_buyers'
-  root "buyers#show_buyers"
+  # get 'show' => 'buyers#show_buyers'
+  # get 'new' => 'buyers#new'
+  # put 'forms' => 'buyers#create_buyers'
+  # root "buyers#show_buyers"
+ 
+  resources :buyers
+  resources :constructions
+  resources :databases
+  resources :easements
+  resources :liens
+  resources :manufactured_homes
+  resources :mortgages
+  resources :occupancies
+  resources :prior_policies
+  resources :profiles
+  resources :property_uses
+  resources :taxes
 
 end
