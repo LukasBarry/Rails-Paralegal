b5ea695a636b8475b7cfebaedf289a53d82fd8c5
Brian McDonald <bmcdonald05@gmail.com>
2016-05-13 17:34:45 -0400

    fixed buyer id issue in schema

--- db/schema.rb
+++ db/schema.rb
@@ -40,10 +40,10 @@ ActiveRecord::Schema.define(version: 20160513152047) do
     t.string   "construct_lien_name"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
-    t.integer  "buyer_id_id"
+    t.integer  "buyer_id"
   end
 
-  add_index "constructions", ["buyer_id_id"], name: "index_constructions_on_buyer_id_id"
+  add_index "constructions", ["buyer_id"], name: "index_constructions_on_buyer_id"
 
   create_table "easements", force: :cascade do |t|
     t.string   "exists"
@@ -54,20 +54,20 @@ ActiveRecord::Schema.define(version: 20160513152047) do
     t.string   "recorded_easement_content_type"
     t.integer  "recorded_easement_file_size"
     t.datetime "recorded_easement_updated_at"
-    t.integer  "buyer_id_id"
+    t.integer  "buyer_id"
   end
 
-  add_index "easements", ["buyer_id_id"], name: "index_easements_on_buyer_id_id"
+  add_index "easements", ["buyer_id"], name: "index_easements_on_buyer_id"
 
   create_table "liens", force: :cascade do |t|
     t.text     "liens"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "lien_amount"
-    t.integer  "buyer_id_id"
+    t.integer  "buyer_id"
   end
 
-  add_index "liens", ["buyer_id_id"], name: "index_liens_on_buyer_id_id"
+  add_index "liens", ["buyer_id"], name: "index_liens_on_buyer_id"
 
   create_table "manufactured_homes", force: :cascade do |t|
     t.string   "m_home_exists"
@@ -78,10 +78,10 @@ ActiveRecord::Schema.define(version: 20160513152047) do
     t.string   "m_home_affidavit_content_type"
     t.integer  "m_home_affidavit_file_size"
     t.datetime "m_home_affidavit_updated_at"
-    t.integer  "buyer_id_id"
+    t.integer  "buyer_id"
   end
 
-  add_index "manufactured_homes", ["buyer_id_id"], name: "index_manufactured_homes_on_buyer_id_id"
+  add_index "manufactured_homes", ["buyer_id"], name: "index_manufactured_homes_on_buyer_id"
 
   create_table "mortgages", force: :cascade do |t|
     t.string   "owner"
@@ -89,21 +89,21 @@ ActiveRecord::Schema.define(version: 20160513152047) do
     t.integer  "book"
     t.integer  "page"
     t.integer  "amount"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "buyer_id_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "buyer_id"
   end
 
-  add_index "mortgages", ["buyer_id_id"], name: "index_mortgages_on_buyer_id_id"
+  add_index "mortgages", ["buyer_id"], name: "index_mortgages_on_buyer_id"
 
   create_table "occupancies", force: :cascade do |t|
     t.string   "occupants"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "buyer_id_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "buyer_id"
   end
 
-  add_index "occupancies", ["buyer_id_id"], name: "index_occupancies_on_buyer_id_id"
+  add_index "occupancies", ["buyer_id"], name: "index_occupancies_on_buyer_id"
 
   create_table "prior_policies", force: :cascade do |t|
     t.string   "pp_exists"
@@ -113,29 +113,29 @@ ActiveRecord::Schema.define(version: 20160513152047) do
     t.string   "pp_copy_content_type"
     t.integer  "pp_copy_file_size"
     t.datetime "pp_copy_updated_at"
-    t.integer  "buyer_id_id"
+    t.integer  "buyer_id"
   end
 
-  add_index "prior_policies", ["buyer_id_id"], name: "index_prior_policies_on_buyer_id_id"
+  add_index "prior_policies", ["buyer_id"], name: "index_prior_policies_on_buyer_id"
 
   create_table "property_uses", force: :cascade do |t|
     t.string   "property_use_type"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
-    t.integer  "buyer_id_id"
+    t.integer  "buyer_id"
   end
 
-  add_index "property_uses", ["buyer_id_id"], name: "index_property_uses_on_buyer_id_id"
+  add_index "property_uses", ["buyer_id"], name: "index_property_uses_on_buyer_id"
 
   create_table "taxes", force: :cascade do |t|
     t.integer  "paid_through"
     t.integer  "amount"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.integer  "buyer_id_id"
+    t.integer  "buyer_id"
   end
 
-  add_index "taxes", ["buyer_id_id"], name: "index_taxes_on_buyer_id_id"
+  add_index "taxes", ["buyer_id"], name: "index_taxes_on_buyer_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
