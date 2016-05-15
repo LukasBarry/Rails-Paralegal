60629c64fd17f24fdb0f1ca9e8e3dc155819ce2f
Annette Myers <annette.myers12@gmail.com>
2016-05-15 10:44:53 -0400

    upload for pdf

--- app/models/easement.rb
+++ app/models/easement.rb
@@ -1,5 +1,8 @@
 class Easement < ActiveRecord::Base
+  belongs_to :buyer
   has_attached_file :recorded_easement, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :recorded_easement, content_type: /\Aimage\/.*\Z/
-  belongs_to :buyer
+
+  has_attached_file :uploaded_file
+  validates_attachment_content_type :uploaded_file, :content_type =>['application/pdf']
 end
--- app/models/manufactured_home.rb
+++ app/models/manufactured_home.rb
@@ -1,5 +1,8 @@
 class ManufacturedHome < ActiveRecord::Base
+  belongs_to :buyer
   has_attached_file :m_home_affidavit, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :m_home_affidavit, content_type: /\Aimage\/.*\Z/
-  belongs_to :buyer
+
+  has_attached_file :uploaded_file
+  validates_attachment_content_type :uploaded_file, :content_type =>['application/pdf']
 end
