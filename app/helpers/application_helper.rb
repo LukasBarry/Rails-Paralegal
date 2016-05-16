65f6643834499ce641b21470d008d67240011de8
Lukas <Lukasbarry@yahoo.com>
2016-05-16 13:14:23 -0400

    routes fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def buyer_check
+     if @buyer.present?
+       eval("link_to 'Edit Borrower', edit_buyer_path(@buyer.id)")
+     else
+       eval("link_to 'New Borrower', new_buyer_path")
+   end
+ end
+ def link_check(item)
+   if eval("@buyer.#{item}").present?
+     eval("link_to 'Edit #{item.titleize.gsub('_', ' ')}', edit_buyer_#{item}_path(@buyer.id, @buyer.#{item})")
+   else
+     eval("link_to 'New #{item.titleize.gsub('_', ' ')}', new_buyer_#{item}_path(@buyer.id)")
+   end
+  end
 end
