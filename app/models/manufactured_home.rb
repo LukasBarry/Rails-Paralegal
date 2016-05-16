50ed21cc98dac161a9592690a344b5248292eb1e
Annette Myers <annette.myers12@gmail.com>
2016-05-16 11:01:00 -0400

    PDF upload

--- app/models/manufactured_home.rb
+++ app/models/manufactured_home.rb
@@ -1,8 +1,5 @@
 class ManufacturedHome < ActiveRecord::Base
   belongs_to :buyer
-  has_attached_file :m_home_affidavit, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :m_home_affidavit, content_type: /\Aimage\/.*\Z/
-
-  has_attached_file :uploaded_file
-  validates_attachment_content_type :uploaded_file, :content_type =>['application/pdf']
+  has_attached_file :m_home_affidavit
+  validates_attachment_content_type :m_home_affidavit, :content_type => ['application/pdf']
 end
--- app/models/prior_policy.rb
+++ app/models/prior_policy.rb
@@ -1,8 +1,5 @@
 class PriorPolicy < ActiveRecord::Base
   belongs_to :buyer
-  has_attached_file :pp_copy, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :pp_copy, content_type: /\Aimage\/.*\Z/
-
-  has_attached_file :uploaded_file
-  validates_attachment_content_type :uploaded_file, :content_type =>['application/pdf']
+  has_attached_file :pp_copy
+  validates_attachment_content_type :pp_copy, :content_type =>['application/pdf']
 end
