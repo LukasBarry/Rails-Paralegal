23fec0ba0d1350e47ec6742173faeaa5b39e3a56
Lukas <Lukasbarry@yahoo.com>
2016-05-17 10:26:34 -0400

    database work

--- app/controllers/databases_controller.rb
+++ app/controllers/databases_controller.rb
@@ -1,11 +1,11 @@
 class DatabasesController < ApplicationController
   def index
-    @database = Database.order(:primary_owner)
-    respond_to do |format|
-      format.html
-      format.csv { send_data @database.to_csv }
-      format.xls # { send_data @database.to_csv(col_sep: "\t") }
-    end
+    @database = Database.order(:street_number, :street_name)
+    # respond_to do |format|
+    #   format.html
+      # format.csv { send_data @database.to_csv }
+      # format.xls { send_data @database.to_csv(col_sep: "\t") }
+    # end
   end
 
   def import
