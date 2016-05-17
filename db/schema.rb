1f411c0c5543711eaec5875a03fc85d87ad621e4
mithunder <mselvaratnam@gmail.com>
2016-05-17 10:58:40 -0400

    readded migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516151015) do
+ActiveRecord::Schema.define(version: 20160517145606) do
 
   create_table "buyers", force: :cascade do |t|
     t.string   "current_owner"
