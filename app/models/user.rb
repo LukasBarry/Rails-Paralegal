8c2cbfa12fd029a9cf15c6636a961d79a051869d
Lukas <Lukasbarry@yahoo.com>
2016-05-17 10:33:38 -0400

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  has_many :buyers
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
-         has_many :buyers
+  has_many :buyers
 end
