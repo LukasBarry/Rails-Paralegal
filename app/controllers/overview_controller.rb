95244d65c7ae1b496e1b2d4e6d5693585df2de11
Brian McDonald <bmcdonald05@gmail.com>
2016-05-18 11:36:07 -0400

    trying to get navbar to work with overview

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -11,4 +11,16 @@ class OverviewController < ApplicationController
 		@property = PropertyUse.find(params[:id])
 		@tax = Tax.find(params[:id])
 	end
+	def new
+		@buyer = Buyer.find(params[:buyer_id])
+		@construction = Construction.find(params[:id])
+		@easement = Easement.find(params[:id])
+		@lien = Lien.find(params[:id])
+		@manufactured = ManufacturedHome.find(params[:id])
+		# @mortgage = Mortgage.find(params[:id])
+		@occupancy = Occupancy.find(params[:id])
+		@prior = PriorPolicy.find(params[:id])
+		@property = PropertyUse.find(params[:id])
+		@tax = Tax.find(params[:id])
+	end
 end
