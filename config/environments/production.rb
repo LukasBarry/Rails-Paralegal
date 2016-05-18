57030ceefd5c50675cc06055817348e3af5f4b93
mithunder <mselvaratnam@gmail.com>
2016-05-18 14:29:00 -0400

    fixing typo in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = {host: 'https://paralegalwake.herokuapp.com/' }
+  config.action_mailer.default_url_options = {host: 'https://paralegalwake.herokuapp.com/'}
   config.action_mailer.default charset: 'utf-8'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
@@ -12,6 +12,7 @@ Rails.application.configure do
     password: ENV['pw'],
     authentication: 'plain',
     enable_starttls_auto: true
+  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
