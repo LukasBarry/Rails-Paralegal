948e85681922d11a7d4a975b57a6c368415d74fe
Lukas <Lukasbarry@yahoo.com>
2016-07-13 13:41:49 -0400

    increase cvs query speed

--- app/helpers/databases_helper.rb
+++ app/helpers/databases_helper.rb
@@ -1,11 +1,16 @@
 module DatabasesHelper
   require 'csv'
   def person(buyer)
-   thing = buyer.split(' ')
-   num = thing.shift
-   csv_text = File.read('property.csv')
-   csv = CSV.parse(csv_text, headers: true)
-   $testing = csv.find{|row| row['Street Name'] == thing.join(' ') && row['Street Number'] == num
-   }
+   num = buyer.split(' ').shift
+   addr = buyer.split(' ').pop
+   $testing = []
+   CSV.foreach('property.csv', headers: true) do |row|
+     $testing<<row['Street Name'] == addr && row['Street Number'] == num
+   end
   end
 end
+
+  #  csv_text = File.read('property.csv')
+  #  csv = CSV.parse(csv_text, headers: true)
+  #  $testing = csv.find{|row| row['Street Name'] == thing.join(' ') && row['Street Number'] == num
+  #  }
